6964a735543dc39ab1cdd91ba981f9f34e323f04
Laurent Fortin <laurent.fortin@gmail.com>
2025-05-25 10:02:30 -0400

    fixed DOM ready handler

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ import './globals'; // Sets globals for jQuery, Underscore, Backbone, and Handle
 import router from './AppRouter.js';
 
 Backbone.history.start(); // Enable routing
+
+// DOM Ready
+$(function () {
+  // Initialize custom jQuery plugins or behaviors
+});
