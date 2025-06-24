d8efd4e37bb9c00bdf02c10ac04ad4f57c28f4bf
Laurent Fortin <laurent.fortin@gmail.com>
2025-06-24 10:43:30 -0400

    added view for not found route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,11 +2,13 @@
 import HomeView from "../views/HomeView.js";
 import AboutView from "../views/AboutView.js";
 import HelpView from "../views/HelpView.js";
+import NotFoundView from "../views/NotFoundView.js";
 import { usecases } from "../data/usecases.js";
 
 const home = new HomeView();
 const about = new AboutView();
 const help = new HelpView();
+const notFound = new NotFoundView();
 
 const AppRouter = Backbone.Router.extend({
   routes: {
@@ -29,12 +31,7 @@ const AppRouter = Backbone.Router.extend({
   },
 
   notFound() {
-    $("#app").html(`
-      <div class="container mt-4">
-        <h2 class="text-danger"><i class="bi bi-exclamation-triangle"></i> 404 Not Found</h2>
-        <p>The page you requested could not be found.</p>
-      </div>
-    `);
+    notFound.render();
   },
 });
 
---
+++ src/views/NotFoundView.js
@@ -0,0 +1,17 @@
+// NotFoundView.js
+const NotFoundView = Backbone.View.extend({
+  el: "#app",
+
+  render() {
+    const html = `
+      <div class="container mt-4">
+        <h2 class="text-danger"><i class="bi bi-exclamation-triangle"></i> 404 Not Found</h2>
+        <p>The page you requested could not be found.</p>
+      </div>
+    `;
+
+    this.$el.html(html);
+  },
+});
+
+export default NotFoundView;
