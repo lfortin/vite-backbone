a44976fd51b847edd628dd05bbef6a4a54641965
Laurent Fortin <laurent.fortin@gmail.com>
2026-02-28 10:36:52 -0500

    views: moved html const outside of render()

--- src/views/HelpView.js
+++ src/views/HelpView.js
@@ -1,17 +1,17 @@
 // HelpView.js
+const html = `
+  <div class="container mt-4">
+    <h2>Help</h2>
+    <p>Need help? Create an issue on the <a href="https://github.com/lfortin/vite-backbone" target="_blank">GitHub repo</a> or check the <a href="https://backbonejs.org/#Getting-started" target="_blank">Backbone.js docs</a>.</p>
+  </div>
+`;
+
 const HelpView = Backbone.View.extend({
   id: "help",
 
   tagName: "div",
 
   render() {
-    const html = `
-      <div class="container mt-4">
-        <h2>Help</h2>
-        <p>Need help? Create an issue on the <a href="https://github.com/lfortin/vite-backbone" target="_blank">GitHub repo</a> or check the <a href="https://backbonejs.org/#Getting-started" target="_blank">Backbone.js docs</a>.</p>
-      </div>
-    `;
-
     this.$el.html(html);
     return this;
   },
--- src/views/NotFoundView.js
+++ src/views/NotFoundView.js
@@ -1,17 +1,17 @@
 // NotFoundView.js
+const html = `
+  <div class="container mt-4">
+    <h2 class="text-danger"><i class="bi bi-exclamation-triangle"></i> 404 Not Found</h2>
+    <p>The page you requested could not be found.</p>
+  </div>
+`;
+
 const NotFoundView = Backbone.View.extend({
   id: "not-found",
 
   tagName: "div",
 
   render() {
-    const html = `
-      <div class="container mt-4">
-        <h2 class="text-danger"><i class="bi bi-exclamation-triangle"></i> 404 Not Found</h2>
-        <p>The page you requested could not be found.</p>
-      </div>
-    `;
-
     this.$el.html(html);
     return this;
   },
