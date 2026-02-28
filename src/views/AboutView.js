b0c1f5099f8aa94f086ec1d9581ec8476b7c4d5b
Laurent Fortin <laurent.fortin@gmail.com>
2026-02-28 10:36:52 -0500

    views: moved html const outside of render()

--- src/views/AboutView.js
+++ src/views/AboutView.js
@@ -1,18 +1,18 @@
 // AboutView.js
+const html = `
+  <div class="container mt-4">
+    <h2>About</h2>
+    <p>This is a Vite + Backbone starter template using Bootstrap, Handlebars, and more.</p>
+    <p>Released under the MIT-0 License.</p>
+  </div>
+`;
+
 const AboutView = Backbone.View.extend({
   id: "about",
 
   tagName: "div",
 
   render() {
-    const html = `
-      <div class="container mt-4">
-        <h2>About</h2>
-        <p>This is a Vite + Backbone starter template using Bootstrap, Handlebars, and more.</p>
-        <p>Released under the MIT-0 License.</p>
-      </div>
-    `;
-
     this.$el.html(html);
     return this;
   },
