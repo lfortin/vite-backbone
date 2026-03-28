debd32f937e7efd4e18231def0d3ef85ac1fb34f
Laurent Fortin <laurent.fortin@gmail.com>
2026-03-28 11:10:55 -0400

    improved tests/setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -3,11 +3,11 @@ import $ from "jquery";
 import _ from "underscore";
 import Backbone from "backbone";
 import Handlebars from "handlebars";
-global.$ = $;
-global.jQuery = $;
-global._ = _;
-global.Backbone = Backbone;
-global.Handlebars = Handlebars;
+window.$ = $;
+window.jQuery = $;
+window._ = _;
+window.Backbone = Backbone;
+window.Handlebars = Handlebars;
 
 // Add root element to the DOM
 document.body.innerHTML = '<div id="app"></div>';
