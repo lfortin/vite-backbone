b8f8db773de60bce54f918af227c46fc32309c56
Laurent Fortin <laurent.fortin@gmail.com>
2026-03-29 16:40:14 -0400

    main.js, main-cdn.js: rearranged imports

--- src/views/HelpView.js
+++ src/views/HelpView.js
@@ -6,8 +6,8 @@ const content = html`
     <h2>Help</h2>
     <p>
       Need help? Create an issue on the
-      <a href="https://github.com/lfortin/vite-backbone" target="_blank">GitHub repo</a> or check the
-      <a href="https://backbonejs.org/#Getting-started" target="_blank">Backbone.js docs</a>.
+      <a href="https://github.com/lfortin/vite-backbone" target="_blank">GitHub repo</a> or check
+      the <a href="https://backbonejs.org/#Getting-started" target="_blank">Backbone.js docs</a>.
     </p>
   </div>
 `;
